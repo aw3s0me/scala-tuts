327b9ce4d6dd058d065b74f36a53f7b5f68b4669
aw3s0me <akorovin00@gmail.com>
2016-12-21 08:02:18 +0100

    printing out strings

--- scala_tuts/src/TestOut.scala
+++ scala_tuts/src/TestOut.scala
@@ -1,6 +1,21 @@
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
 /**
   * Created by akorovin on 21.12.2016.
   */
 object TestOut {
+  def main(args: Array[String]): Unit = {
+    val name = "Alex"
+    val age = 23
+    val weight = 65.5
 
+    // printing out variables. need "s" in front of string
+    println(s"Hello $name")
+
+    // print out formatted. $age+1 - to calculate + 1 age, weight.2 for double decimal
+    println(f"I am ${age + 1} and weight $weight%.2f")
+  }
 }
