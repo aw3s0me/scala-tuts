05f5c205a4ff0e9e11916e0ef1d73381a622a597
aw3s0me <akorovin00@gmail.com>
2016-12-23 19:20:48 +0100

    tuples comments

--- scala_tuts/src/TestTuples.scala
+++ scala_tuts/src/TestTuples.scala
@@ -1,10 +1,18 @@
 /**
   * Created by akorovin on 21.12.2016.
+  * NOTE: THEY ARE NOT PRIMITIVE TYPES
+  * NOTE: THEY ARE CLASSES!
+  * JUST SYNTACTICAL SUGAR
+  * NOTE: MAX 22 TUPLES AVAILABLE (max 22 PARAMETERS)
+  * NOTE: can change values only by copying
   */
 object TestTuples {
   def main(args: Array[String]): Unit = {
-    // INSTANTIATE TUPLE
-    var tupleMarge = (103, "Marge Simpson", 10.25)
+    // INSTANTIATE TUPLE. TUPLE3 type (3 PARAMETERS)
+    val tupleMarge: (Int, String, Double) = (103, "Marge Simpson", 10.25)
+    // SAME AS
+    val tupleMarge2: (Int, String, Double) = Tuple3(103, "Marge Simpson", 10.25)
+
     // ACCESS TUPLE by underscore
     printf("%s owes us $%.2f\n", tupleMarge._2, tupleMarge._3)
     // ITERATE through each element in tuple
